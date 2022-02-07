5bbda54aab09a1df3fb41d5dd3612078cbaf4e55
Gaizgrol <gabrielizotongo@gmail.com>
2022-02-07 20:01:15 -0300

    correçao no modelo de projeto

--- teste.js
+++ teste.js
@@ -1,4 +1,8 @@
-
+/**
+ * Função que soma todos os parâmetros
+ * @param  {number[]} nums 
+ * @returns {number}
+ */
 const soma = function ( ...nums ) {
     let valor = 0;
     for ( let i=0; i<nums.length; i++ )
